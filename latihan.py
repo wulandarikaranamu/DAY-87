081db31f70aa4da7e76dbd222d11af3c4c176897
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-03-08 21:20:36 +0700

    komentar

---
+++ latihan.py
@@ -0,0 +1,18 @@
+#menampilkan type data
+nama = "merry"
+umur = 18
+tinggi = 1.63
+status = True
+
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+print(type(status))
+
+#operator not dalam python
+a = False
+b = True
+c = not a
+print(a,c)
+c = not b
+print(b,c)
